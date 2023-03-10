3690ecf043e520611cf01549a3731b87a4f8979f
Jules Moreno <julesmoreno66@gmail.com>
2023-03-10 14:29:20 +0800

    fix bug

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,9 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
         // Make sure it's always the last config, so it gets the chance to override other configs.
-        'eslint-config-prettier',
+        
+        "airbnb-typescript",
+        "prettier"
     ],
     "overrides": [
     ],
